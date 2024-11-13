3a8d9cfa8cc819040804b6d32cbc1575b46ed472
Vraj Patel <vrajpatel24905@gmail.com>
2024-11-13 08:09:50 +0530

    Leetcode Problem 2563 solved

---
+++ 2563.java
@@ -0,0 +1,26 @@
+class Solution {
+    public long countFairPairs(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        int i = 0;
+        long c = 0;
+        while(i < nums.length){
+            int j = binarySearch(nums, lower - nums[i],i+1);
+            int k = binarySearch(nums, upper - nums[i] + 1,i+1);
+            c +=(k-j);
+            i++;
+        }
+        return c;
+    }
+    public int binarySearch(int[] matrix, int x , int lo) {
+        int hi = matrix.length; 
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            if (matrix[mid] >= x) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return lo;
+    }
+}
